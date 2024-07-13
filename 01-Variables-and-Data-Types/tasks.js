a966c7d3f5d6cccb16eec4196c09b9502dfb7b89
Chitraksh Tarun <65770420+ChitrakshTarun@users.noreply.github.com>
2024-07-14 00:28:40 +0530

    (day 1) variables and data types

---
+++ 01-Variables-and-Data-Types/tasks.js
@@ -0,0 +1,27 @@
+// Task 1
+var num = 23;
+console.log(num);
+// Task 2
+let str = "Chitraksh";
+console.log(str);
+// Task 3
+const boolVariable = true;
+console.log(boolVariable);
+// Task 4
+let a = 23;
+let b = "Chitraksh";
+let c = false;
+let d = {apple: "red", mango: "yellow"};
+let e = [1, 2, 3, 4, 5, 6];
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+// Task 5
+let test = "Initial value";
+console.log(`test is currently set to ${test}`);
+test = "Reassigned value";
+console.log(`test is currently set to ${test}`);
+// Task 6
+boolVariable = false;
